70bc654a31662fe6cfc19c75e00075daa8b67e31
fran <francisco.chiarino@gmail.com>
2020-05-24 19:54:36 +0200

    Save before bringing in components

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 
 import React from "react"
-import App from "./components/App.js"
+import App from "./components/home/App.js"
 import reactDOM from "react-dom"
 import { Provider } from 'react-redux';
 import store from './store';
-import './style/main.scss';
+
 
 reactDOM.render(<Provider store={store}><App/></Provider>,document.getElementById("root"))
