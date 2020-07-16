4fbc8c8137aea1a68814d9395b6ba649feb90dde
fran <francisco.chiarino@gmail.com>
2020-07-16 13:11:21 +0200

    Made Nav responsive

--- client/src/components/home/Nav.js
+++ client/src/components/home/Nav.js
@@ -1,24 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink, Link } from 'react-router-dom'
 
 export default function Nav({ user, setLogin, openSignUp }) {
+  const [mobileNav, setMobileNav] = useState('mobileNavClose');
+
+  const handleMobileNav = () => {
+    mobileNav === 'mobileNavOpen' ? setMobileNav('mobileNavClose') : setMobileNav('mobileNavOpen');
+  };
+
+  const handleMobileNavAndLogin = () => {
+    handleMobileNav();
+    setLogin(true);
+  }
+
+  const handleMobileNavAndSignUp = () => {
+    handleMobileNav();
+    openSignUp();
+  }
+
   return (
-    <nav>
-      <div>
-          <NavLink className="navLink" activeClassName="navActive" to='/' exact={true} >HOME</NavLink>
-          <NavLink className="navLink" activeClassName="navActive" to='/books/favorites'>FAVORITES</NavLink>
-          <NavLink className="navLink" activeClassName="navActive" to='/books/fiction'>FICTION</NavLink>
-          <NavLink className="navLink" activeClassName="navActive" to='/books/mystery'>MYSTERY</NavLink>
-          <NavLink className="navLink" activeClassName="navActive" to='/books/tech'>TECH</NavLink>
-          <NavLink className="navLink" activeClassName="navActive" to='/books/biography'>BIOGRAPHY</NavLink>
-      </div>
-      <div>
-          {user && user.email ? 
-              <button className="userLink"><Link to='/users/user'>ACCOUNT</Link></button> : 
-              <button className="userLink" onClick={() => setLogin(true)}>LOG IN</button>
-          }
-          <button className="userLink" onClick={openSignUp}>SIGN UP</button>
-      </div>
-  </nav>
+    <>
+      <button id="menuBtn" onClick={handleMobileNav}>Menu</button>
+      <nav className={mobileNav}>
+        <div className="navLinks">
+            <NavLink className="navLink" activeClassName="navActive" onClick={handleMobileNav} to='/' exact={true} >HOME</NavLink>
+            <NavLink className="navLink" activeClassName="navActive" onClick={handleMobileNav} to='/books/favorites'>FAVORITES</NavLink>
+            <NavLink className="navLink" activeClassName="navActive" onClick={handleMobileNav} to='/books/fiction'>FICTION</NavLink>
+            <NavLink className="navLink" activeClassName="navActive" onClick={handleMobileNav} to='/books/mystery'>MYSTERY</NavLink>
+            <NavLink className="navLink" activeClassName="navActive" onClick={handleMobileNav} to='/books/tech'>TECH</NavLink>
+            <NavLink className="navLink" activeClassName="navActive" onClick={handleMobileNav} to='/books/biography'>BIOGRAPHY</NavLink>
+        </div>
+        <div className="navButtons">
+            {user && user.email ? 
+                <button onClick={handleMobileNav} className="userLink"><Link to='/users/user'>ACCOUNT</Link></button> : 
+                <button onClick={handleMobileNavAndLogin} className="userLink">LOG IN</button>
+            }
+            <button className="userLink" onClick={handleMobileNavAndSignUp}>SIGN UP</button>
+        </div>
+      </nav>
+    </>
   )
 }
