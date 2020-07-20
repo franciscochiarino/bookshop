5340411b7a22d4f55ae1c84a988d29e2e81fa918
fran <francisco.chiarino@gmail.com>
2020-07-20 14:41:16 +0200

    Build #5

---
+++ client/build/precache-manifest.c4f6b6a36765e60e457d322a045a008c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e52f76a22928a4ab647e709fba0f6c8c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6a6b7ea71afeffb0961",
+    "url": "/static/css/main.6bb2066a.chunk.css"
+  },
+  {
+    "revision": "eedb870aa7e5fe9e541b",
+    "url": "/static/js/2.f5c178ed.chunk.js"
+  },
+  {
+    "revision": "90d09d9301b52b8b5ab0294c55a24973",
+    "url": "/static/js/2.f5c178ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6a6b7ea71afeffb0961",
+    "url": "/static/js/main.81e37717.chunk.js"
+  },
+  {
+    "revision": "e2afce1df808a6d0526b",
+    "url": "/static/js/runtime-main.f988630a.js"
+  },
+  {
+    "revision": "21fd01685405ed772aa2a7b4c75cbc73",
+    "url": "/static/media/books-1097_231.21fd0168.png"
+  },
+  {
+    "revision": "6802ad51befbeaec636e05aa87612006",
+    "url": "/static/media/hero_1920x1280.6802ad51.jpg"
+  },
+  {
+    "revision": "3c121c21f871fddea42e82cd044f8d2f",
+    "url": "/static/media/node-logo.3c121c21.png"
+  },
+  {
+    "revision": "705fec42cf87f36b654c33384cd26326",
+    "url": "/static/media/react-logo.705fec42.png"
+  },
+  {
+    "revision": "9fdd7a84d81d52724e37317848468d70",
+    "url": "/static/media/redux-logo.9fdd7a84.png"
+  },
+  {
+    "revision": "ac774bc1e011e375efca650139abdcd5",
+    "url": "/static/media/sass-logo.ac774bc1.png"
+  }
+]);
